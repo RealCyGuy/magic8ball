5610871bf3282f12ba125af618c12ba1d5507bc3
RealCyGuy <54488650+RealCyGuy@users.noreply.github.com>
2020-01-09 17:43:58 -0800

    hi

---
+++ README.md
@@ -0,0 +1,3 @@
+# magic8ball package
+
+Gets a list of magic 8 ball responses.
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="magic8ball-realcyguy",
+    version="1.0",
+    author="Cyrus Yip",
+    author_email="cyruscmyip1@gmail.com",
+    description="Magic 8 ball answers.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/realcyguy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
